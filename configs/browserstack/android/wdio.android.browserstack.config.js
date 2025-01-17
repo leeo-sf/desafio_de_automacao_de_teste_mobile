708412b7c4de2e7c2c95b669823952bfad585bd3
Leonardo Ferreira <leonardoferreira032@gmail.com>
2025-01-17 01:36:41 -0300

    fix config is not defined browserstack

--- configs/browserstack/android/wdio.android.browserstack.config.js
+++ configs/browserstack/android/wdio.android.browserstack.config.js
@@ -1,61 +1,54 @@
 require("dotenv").config();
+const { config } = require("../../wdio.shared.config")
+require("dotenv").config();
 
-exports.config = {
-    environment: 'BrowserStack',
+config.environment = 'BrowserStack'
+config.user = process.env.BROWSERSTACK_USER
+config.key = process.env.BROWSERSTACK_KEY
 
-    services: ['browserstack'],
-    appium: {
-        command: 'appium'
-    },
+config.protocolTimeout = 120000
 
-    specs: [
-        '../../../tests/**/*.js'
-    ],
+config.services = ['browserstack']
+config.appium = {
+    command: 'appium'
+}
 
-    user: process.env.BROWSERSTACK_USER,
-    key: process.env.BROWSERSTACK_KEY,
+config.specs = [
+    '../../../tests/swipe/*.js'
+]
 
-    capabilities: [
-        {
-            'appium:automationName': 'UiAutomator2',
-            'appium:deviceName': 'Samsung Galaxy M52',
-            'appium:platformVersion': '13.0',
-            platformName: 'Android',
-            'appium:app': process.env.APP_ID,
-            'appium:deviceName': 'Google Pixel 6',
-            'appium:platformVersion': '12.0',
-            'appium:appPackage': 'com.wdiodemoapp',
-            'appium:autoGrantPermissions': true,
-            'appium:browserstack.debug': true
-        }
-    ],
+config.capabilities = [
+    {
+        'appium:automationName': 'UiAutomator2',
+        'appium:deviceName': 'Samsung Galaxy M52',
+        'appium:platformVersion': '13.0',
+        platformName: 'Android',
+        'appium:app': process.env.APP_ID,
+        'appium:appPackage': 'com.wdiodemoapp',
+        'appium:autoGrantPermissions': true,
+        'appium:browserstack.debug': true
+    }
+]
 
-    framework: 'mocha',
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000,
-    },
-    reporters: [
-        'spec',
-        ['allure', {
-            outputDir: 'allure-results/browserstack',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false,
-            reportedEnvironmentVars: {
-                Environment: "BrowserStack",
-                Platform: 'Android',
-                DeviceName: config.capabilities[0]['appium:deviceName'],
-                DeviceVersion: config.capabilities[0]['appium:platformVersion']
-            },
-        }]
-    ],
+config.framework = 'mocha'
+config.mochaOpts = {
+    ui: 'bdd',
+    timeout: 60000,
+}
 
-    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
-        await driver.takeScreenshot();
-    },
-};
+config.reporters = [
+    'spec',
+    ['allure', {
+        outputDir: 'allure-results/browserstack',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+        reportedEnvironmentVars: {
+            Environment: "BrowserStack",
+            Platform: 'Android',
+            DeviceName: config.capabilities[0]['appium:deviceName'],
+            DeviceVersion: config.capabilities[0]['appium:platformVersion']
+        },
+    }]
+]
 
-exports.config.capabilities = exports.config.capabilities.map((cap) => ({
-    ...exports.config.capabilities[0],
-    ...cap,
-}));
+exports.config = config;
